8158831b6ca5e1d53d81856bed2089dab98bdb9f
cloudsony999 <cloudsony999@gmail.com>
2023-04-03 13:40:11 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-angular';
+export class AppComponent implements OnInit{
+  name='NISHA';
+  address='asansole';
+
+  uname=new FormControl('');
+  updateName()
+  {
+    this.uname.setValue('AMITAVA')
+  }
+
+  ngOnInit(){
+    console.log('I am nginit!!!!! '+this.name);
+  }
+
+  constructor()
+  {
+    console.log('I am constructor!!!!!'+this.address);
+  }
+  details(){
+    alert(this.name +' stays in '+this.address);
+  }
+
+
+  
 }
